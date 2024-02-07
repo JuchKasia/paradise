f02ca70eb72f23ff9e0c5569ce4bcf6acf06e593
Katarzyna Juchniewicz <juchkasia@gmail.com>
2024-02-07 21:17:47 +0100

    feat: added year to footer__year

--- paradise/js/main.js
+++ paradise/js/main.js
@@ -1,6 +1,11 @@
 const nav = document.querySelector('.nav');
 const burgerBtn = document.querySelector('.burger-btn');
 const navItems = document.querySelectorAll('.nav__item');
+const navBtnBArs = document.querySelector('.burger-btn__bars');
+const allSections = document.querySelectorAll('.section');
+const footerYear = document.querySelector('.footer__year');
+
+
 
 
 const handleNav = () => {
@@ -42,3 +47,12 @@ function showSlides() {
     slides[slideIndex-1].style.display = "block";  
     setTimeout(showSlides, 2000);
 }
+
+
+// change current year
+const handleCurrentYear = () => {
+    const year = (new Date).getFullYear();
+    footerYear.innerText = year;
+}
+
+handleCurrentYear();
